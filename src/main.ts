0aff4772133eb3376b27af0ada272dbee347a4e9
Esdras Silva <esdraslimasilva83@gmail.com>
2024-03-21 16:34:17 -0300

    Adding post user route

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,52 @@
 import express, { application } from "express";
+import { v4 as uuid } from "uuid";
 
 const server = express();
 server.use(express.json());
 
-//routes
-server.get("/", (req, res) => {
-    res.json("Hello World");
+//types
+type TechnologyType = {
+    title: string;
+    studied: boolean;
+    deadline: Date;
+    created_at: Date;
+};
+
+type UserType = {
+    id: string;
+    name: string;
+    username: string;
+    tencologies: TechnologyType[];
+};
+
+// state
+const allUsers: UserType[] = [];
+
+server.post("/user", (req, res) => {
+    const { name, username } = req.body as { name: string; username: string };
+
+    // checking if user already exits
+    const userFound = allUsers.find((usr) => usr.username === username);
+    if (userFound)
+        return res.status(400).json({
+            ok: false,
+            error: `User with username '${username}' already exists!`,
+        });
+
+    const novoUser: UserType = {
+        id: uuid(),
+        name,
+        username,
+        tencologies: [],
+    };
+
+    allUsers.push(novoUser);
+
+    res.status(201).json({
+        ok: true,
+        message: "User created sucessfully",
+        data: novoUser,
+    });
 });
 
 const PORT = 3333;
